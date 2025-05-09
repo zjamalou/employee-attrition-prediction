d4f5506ea128fefed0f64b78062bcebb2c734c0e
zahrajamalou <zahrajamalou91@gmail.com>
2025-05-09 19:11:34 +0330

    initial commit -Employee Attrition Prediction

---
+++ practicepython.py
@@ -0,0 +1,45 @@
+# inp = str(input("enter a num"))
+# strinp = inp[::-1]
+# strint = int(strinp)
+# print(2*strint)
+
+
+# num = int(input("enter a number: "))
+# ones = num % 10
+# num = num - ones
+# print(num + 10)
+
+# dtime = float(input("enter a time value in minutes:"))
+# hour = dtime // 60
+# min = dtime % 60
+# print(f"{hour} hour and {min} minutes")
+
+
+# num = input("enter a number:")
+# dsum = 0
+# for i in num:
+#     dsum += int(i)
+# print(dsum)
+
+# num = int(input("enter a age number:"))
+
+# if 0 < num < 6 :
+#     print("khordsal")
+
+# elif 6 <= num < 10 :
+#     print("koodak")
+
+# elif 10 <= num < 14 :
+#     print("nojavan")
+
+# elif 14 <= num < 24  :
+#     print("javan")
+# elif 24 <= num < 40:
+#     print("bozorgsal")
+
+# elif num >= 40:
+#     print("miansal")
+
+
+num = [4,7,8,12,36]
+print(sum(num))
